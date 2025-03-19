1cc2aa1313b4ddf73207464a125ffddf3ac20844
Emil Rasmus Merzin <rasmusmerzin@gmail.com>
2025-03-19 13:31:16 +0200

    HelloController

---
+++ src/main/java/dev/merzin/forum/HelloController.java
@@ -0,0 +1,12 @@
+package dev.merzin.forum;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@GetMapping("/")
+	public String hello() {
+		return "Online";
+	}
+}
