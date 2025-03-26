33929aa48e9a323f0cb96be634cfbd2339b6318d
Emil Rasmus Merzin <rasmusmerzin@gmail.com>
2025-03-26 17:17:43 +0200

    web: me posts component

--- web/src/app/me/me.component.ts
+++ web/src/app/me/me.component.ts
@@ -2,63 +2,23 @@ import { Component, inject, OnDestroy, OnInit } from "@angular/core";
 import { NavigationComponent } from "../navigation/navigation.component";
 import { HeaderComponent } from "./header/header.component";
 import { AuthenticationService } from "../authentication/authentication.service";
-import { Router, RouterOutlet } from "@angular/router";
-import { Post } from "../post/post";
-import { PostService } from "../post/post.service";
-import { PostCardComponent } from "../post/post-card/post-card.component";
-import { SpinnerComponent } from "../spinner/spinner.component";
+import { RouterOutlet } from "@angular/router";
+import { PostsComponent } from "./posts/posts.component";
 
 @Component({
   selector: "app-me",
-  imports: [
-    RouterOutlet,
-    NavigationComponent,
-    HeaderComponent,
-    PostCardComponent,
-    SpinnerComponent,
-  ],
+  imports: [RouterOutlet, NavigationComponent, HeaderComponent, PostsComponent],
   templateUrl: "./me.component.html",
   styleUrl: "./me.component.scss",
 })
 export class MeComponent implements OnInit, OnDestroy {
-  router = inject(Router);
   authenticationService = inject(AuthenticationService);
-  postService = inject(PostService);
-
-  posts?: Post[];
-  loading = true;
-  before = "";
 
   ngOnInit() {
     setTimeout(() => this.authenticationService.checkLogin());
-    this.loadMorePosts();
-  }
-
-  async loadMorePosts() {
-    const username = this.authenticationService.getUsername();
-    if (!username) return;
-    try {
-      this.loading = true;
-      const posts = await this.postService.fetchUserPosts(
-        username,
-        this.before,
-      );
-      if (!this.posts) this.posts = posts;
-      else this.posts.push(...posts);
-      if (posts.length === 10) this.before = posts[posts.length - 1].created;
-      else this.before = "";
-    } catch (error) {
-      console.error(error);
-    } finally {
-      this.loading = false;
-    }
   }
 
   ngOnDestroy() {
     this.authenticationService.hideLogin();
-  }
-
-  onPostClick(post: Post) {
-    this.router.navigate(["/post", post.id]);
   }
 }
---
+++ web/src/app/me/posts/posts.component.ts
@@ -0,0 +1,51 @@
+import { Component, inject, OnInit } from "@angular/core";
+import { PostCardComponent } from "../../post/post-card/post-card.component";
+import { SpinnerComponent } from "../../spinner/spinner.component";
+import { Router } from "@angular/router";
+import { AuthenticationService } from "../../authentication/authentication.service";
+import { PostService } from "../../post/post.service";
+import { Post } from "../../post/post";
+
+@Component({
+  selector: "app-me-posts",
+  imports: [PostCardComponent, SpinnerComponent],
+  templateUrl: "./posts.component.html",
+  styleUrl: "./posts.component.scss",
+})
+export class PostsComponent implements OnInit {
+  router = inject(Router);
+  authenticationService = inject(AuthenticationService);
+  postService = inject(PostService);
+
+  posts?: Post[];
+  loading = true;
+  before = "";
+
+  ngOnInit() {
+    this.loadMorePosts();
+  }
+
+  async loadMorePosts() {
+    const username = this.authenticationService.getUsername();
+    if (!username) return;
+    try {
+      this.loading = true;
+      const posts = await this.postService.fetchUserPosts(
+        username,
+        this.before,
+      );
+      if (!this.posts) this.posts = posts;
+      else this.posts.push(...posts);
+      if (posts.length === 10) this.before = posts[posts.length - 1].created;
+      else this.before = "";
+    } catch (error) {
+      console.error(error);
+    } finally {
+      this.loading = false;
+    }
+  }
+
+  onPostClick(post: Post) {
+    this.router.navigate(["/post", post.id]);
+  }
+}
