2921a44de1ed77fb7d6242c37956ff078fd39b8a
Emil Rasmus Merzin <rasmusmerzin@gmail.com>
2025-03-26 22:33:00 +0200

    web: preload auth wallpaper

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,6 +2,8 @@ import { bootstrapApplication } from "@angular/platform-browser";
 import { appConfig } from "./app/app.config";
 import { AppComponent } from "./app/app.component";
 
+new Image().src = "/auth-wall.webp";
+
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err),
 );
