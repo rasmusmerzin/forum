140330703a83f0ca8c19b2c2b12d20581d1a6829
Emil Rasmus Merzin <rasmusmerzin@gmail.com>
2025-03-29 15:10:09 +0200

    web: post page comment list

--- web/src/app/post/post.component.ts
+++ web/src/app/post/post.component.ts
@@ -6,6 +6,9 @@ import { SpinnerComponent } from "../spinner/spinner.component";
 import { HeaderComponent } from "./header/header.component";
 import { PostCardComponent } from "./post-card/post-card.component";
 import { CommentBarComponent } from "./comment-bar/comment-bar.component";
+import { CommentService } from "../comment/comment.service";
+import { Comment } from "../comment/comment";
+import { CommentCardComponent } from "../comment/comment-card/comment-card.component";
 
 @Component({
   selector: "app-post",
@@ -14,6 +17,7 @@ import { CommentBarComponent } from "./comment-bar/comment-bar.component";
     HeaderComponent,
     PostCardComponent,
     CommentBarComponent,
+    CommentCardComponent,
   ],
   templateUrl: "./post.component.html",
   styleUrl: "./post.component.scss",
@@ -21,15 +25,43 @@ import { CommentBarComponent } from "./comment-bar/comment-bar.component";
 export class PostComponent implements OnInit {
   route = inject(ActivatedRoute);
   postService = inject(PostService);
+  commentSerivce = inject(CommentService);
 
   loading = true;
   post?: Post;
+  comments?: Comment[];
+  after = "";
 
   async ngOnInit() {
+    await this.loadPost();
+    await this.loadMoreComments();
+  }
+
+  async loadPost() {
     const { id } = this.route.snapshot.params;
     try {
       this.loading = true;
       this.post = await this.postService.fetchPost(id);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      this.loading = false;
+    }
+  }
+
+  async loadMoreComments() {
+    const { id } = this.route.snapshot.params;
+    try {
+      this.loading = true;
+      const comments = await this.commentSerivce.fetchPostComments(
+        id,
+        this.after,
+      );
+      if (!this.comments) this.comments = comments;
+      else this.comments.push(...comments);
+      if (comments.length === 10)
+        this.after = comments[comments.length - 1].created;
+      else this.after = "";
     } catch (error) {
       console.error(error);
     } finally {
