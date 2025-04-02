733e8d53f3bae540ad7cd1d2b9fd74e9bfa3044b
Emil Rasmus Merzin <rasmusmerzin@gmail.com>
2025-04-02 20:09:36 +0300

    web: fixed tests

--- web/src/app/verify/verify.component.spec.ts
+++ web/src/app/verify/verify.component.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { VerifyComponent } from "./verify.component";
+import { mockClass, Mocked } from "../mock";
+import { ActivatedRoute } from "@angular/router";
 
 describe("VerifyComponent", () => {
   let component: VerifyComponent;
   let fixture: ComponentFixture<VerifyComponent>;
+  let activatedRoute: Mocked<ActivatedRoute>;
 
   beforeEach(async () => {
+    activatedRoute = mockClass(ActivatedRoute);
+    activatedRoute.snapshot = { params: { id: "123" } } as any;
+
     await TestBed.configureTestingModule({
       imports: [VerifyComponent],
+      providers: [{ provide: ActivatedRoute, useValue: activatedRoute }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(VerifyComponent);
